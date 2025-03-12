9cabe9d2517648006bf8225868c5895648652c99
Lydia Carter <lcarter200183@yahoo.com>
2025-03-12 18:23:23 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import { add, subtract, multiply, divide } from "./math-utils.js";
+import greet from "./default-export.js";
+
+console.log(add(5, 6));
+console.log(subtract(1, 3));
+console.log(multiply(6, 9));
+console.log(divide(50, 60));
+
+console.log(greet("Lydia"));
